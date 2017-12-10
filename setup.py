919f94811f4f5932d7ba2e29f74b7f483f506446
LTrainExpress <lwaldo@gmail.com>
2017-12-09 19:44:02 -0500

    Updated URL to point at my repo

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Simon Edgett"
 plugin_author_email = "simon@edgett.bc.ca"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/sedgett/OctoPrint_IFTTTmaker"
+plugin_url = "https://github.com/LTrainExpress/OctoPrint_IFTTTmaker"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
